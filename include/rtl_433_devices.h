4e055c94ce62b3425efa708c8ad7386d7ed5c8ce
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2019-03-14 17:16:37 +0000

    Add Jansite TPMS support

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -128,7 +128,8 @@
     DECL(bt_rain) \
     DECL(bresser_5in1) \
     DECL(digitech_xc0324) \
-    DECL(opus_xt300)
+    DECL(opus_xt300) \
+    DECL(tpms_jansite)
 
 #define DECL(name) extern r_device name;
 DEVICES
